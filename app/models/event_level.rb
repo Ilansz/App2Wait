d17dcda28ae40bc45b219de6a51a93138c255da1
ilan szenker <szenkerilan@gmail.com>
2019-06-30 17:12:40 +0300

    all controllers created

--- app/models/event_level.rb
+++ app/models/event_level.rb
@@ -1,3 +1,5 @@
 class EventLevel < ApplicationRecord
   belongs_to :event
+  validates :desciption, presence: true
+  validates :time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :videos
+  has_many :group_users
 
   mount_uploader :photo, PhotoUploader
   validates :username, presence: true, uniqueness: true
