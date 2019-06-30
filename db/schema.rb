05bf5e7107a9cc855472a05910ea9c5639dbff6e
ilan szenker <szenkerilan@gmail.com>
2019-06-30 12:44:28 +0300

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_092819) do
+ActiveRecord::Schema.define(version: 2019_06_30_090717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "challenges", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "event_levels", force: :cascade do |t|
-    t.string "description"
-    t.integer "time"
-    t.bigint "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_event_levels_on_event_id"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "status"
-    t.string "name"
-    t.string "photo"
-    t.bigint "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_events_on_group_id"
-  end
-
-  create_table "group_users", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_users_on_group_id"
-    t.index ["user_id"], name: "index_group_users_on_user_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "levels", force: :cascade do |t|
-    t.integer "time"
-    t.string "description"
-    t.bigint "challenge_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["challenge_id"], name: "index_levels_on_challenge_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -74,28 +23,8 @@ ActiveRecord::Schema.define(version: 2019_06_30_092819) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "photo"
-    t.float "average_lateness"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "videos", force: :cascade do |t|
-    t.string "tag"
-    t.bigint "challenge_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["challenge_id"], name: "index_videos_on_challenge_id"
-    t.index ["user_id"], name: "index_videos_on_user_id"
-  end
-
-  add_foreign_key "event_levels", "events"
-  add_foreign_key "events", "groups"
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
-  add_foreign_key "levels", "challenges"
-  add_foreign_key "videos", "challenges"
-  add_foreign_key "videos", "users"
 end
