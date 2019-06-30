beb58cce96b72e21be0345fcee4b2cb7973ac8d9
Ben Attali <ben.attali8@gmail.com>
2019-06-30 19:03:13 +0300

    Challenge controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,35 @@
-puts 'Destroying old database...'
-User.destroy_all
-Challenge.destroy_all
-GroupUser.destroy_all
-Group.destroy_all
+# puts 'Destroying old database...'
+# User.destroy_all
+# Challenge.destroy_all
+# GroupUser.destroy_all
+# Group.destroy_all
 
-puts "Generating userts"
+# puts "Generating userts"
 
-# User.create!(email: 'axel@app2wait.com',
-#             password: '123456',
-#             username: 'Cuervo',
-#             remote_photo_url: 'AxelResnik.png',
-#             average_lateness: rand(0..25))
+# # User.create!(email: 'axel@app2wait.com',
+# #             password: '123456',
+# #             username: 'Cuervo',
+# #             remote_photo_url: 'AxelResnik.png',
+# #             average_lateness: rand(0..25))
 
-# User.create!(email: ,
-#             password: '123456',
-#             username: ,
-#             photo: ,
-#             average_lateness: rand(0..25))
+# # User.create!(email: ,
+# #             password: '123456',
+# #             username: ,
+# #             photo: ,
+# #             average_lateness: rand(0..25))
 
-puts "Finished generating users"
+# puts "Finished generating users"
 
-puts "Done seeding!"
+# puts "Done seeding!"
+
+Challenge.create(name: "chicken dance",
+                 photo: "https://source.unsplash.com/random")
+
+Challenge.create(name: "pushups",
+                 photo: "https://source.unsplash.com/random")
+
+Challenge.create(name: "get phone numbers",
+                 photo: "https://source.unsplash.com/random")
+
+Challenge.create(name: "a shot on you",
+                 photo: "https://source.unsplash.com/random")
