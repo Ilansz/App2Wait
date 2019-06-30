f1b4b786abd5e5f33cba0e59785d49106da32def
ilan szenker <szenkerilan@gmail.com>
2019-06-30 17:41:54 +0300

    dependencies done

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,7 @@
 class Challenge < ApplicationRecord
   validates :name, presence: true
   validates :photo, presence: true
-  has_many :levels, :videos
+  has_many :levels, dependant: :destroy
+  has_many :videos
   mount_uploader :photo, PhotoUploader
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  has_many :events
-  has_many :group_users
+  has_many :events, dependant: :destroy
+  has_many :group_users, dependant: :destroy
+  has_many :users, through: :group_users
   validates :name, presence: true
 end
