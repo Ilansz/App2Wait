0c77bb743fc38545561e48005082616d833b9cc9
Axel Resnik <axelresnik@gmail.com>
2019-07-01 11:34:24 +0300

    API KEY

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@ Group.destroy_all
 
 puts "Generating userts"
 
-# User.create!(email: 'axel@app2wait.com',
-#             password: '123456',
-#             username: 'Cuervo',
-#             remote_photo_url: 'AxelResnik.png',
-#             average_lateness: rand(0..25))
+User.create!(email: 'axel@app2wait.com',
+            password: '123456',
+            username: 'Cuervo',
+            remote_photo_url: 'app/assets/images/AxelResnik.png',
+            average_lateness: rand(0..25))
 
 # User.create!(email: ,
 #             password: '123456',
