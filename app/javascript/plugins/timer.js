b44d450a7c2c413a34768166083e7c511fad4ae8
Axel Resnik <axelresnik@gmail.com>
2019-07-03 14:34:06 +0300

    pseudocode

--- app/javascript/plugins/timer.js
+++ app/javascript/plugins/timer.js
@@ -10,6 +10,7 @@ const enableTime = () => {
   const secondTime = document.querySelector('#second-level')
   const lastTime = document.querySelector('#last-level')
   const enable = document.querySelector('#enable')
+
   enable.addEventListener('click', (event) => {
     firstTime.classList.remove("disabled")
   })
@@ -17,3 +18,13 @@ const enableTime = () => {
 
 
 export { enableTime }
+// 1. get time of click
+// 2. 3 different variables with  added relevant time (5, 15, 30 min)
+// 3. 1 sec interval for clock.
+// 4. compare future(challenge finish time) and current time
+// 5. and trigger class remove/ add for disabled finished challenge
+// 6. display clock in nice way
+// 7. 2 different times -> challenge finish time, current countdown
+//    ---> subtract future millis from current millis
+//    ---> configure back to mins / seconds
+// 8. remove interval once time finished
