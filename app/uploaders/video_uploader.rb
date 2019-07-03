a3402db012dbb3a545f15cbab0ea59d00b4788c0
ilan szenker <szenkerilan@gmail.com>
2019-07-03 11:30:47 +0300

    added carrierwave

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,4 +1,6 @@
 class VideoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
