bb83f0ecb1d7d486a05d612330576cc5f84ee360
ilan szenker <szenkerilan@gmail.com>
2019-07-07 13:23:02 +0300

    selector done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { toggleForm } from '../plugins/formToggle'
 import { plusMinus } from '../plugins/plusMinus'
+import { selector } from '../plugins/selector'
 
 toggleForm();
 plusMinus();
+selector();
