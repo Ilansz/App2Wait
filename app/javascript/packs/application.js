d34a9a18f798e3db86e2d4db7bf4ec8934e90e51
Ben Attali <ben.attali8@gmail.com>
2019-07-07 14:08:49 +0300

    events edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import { toggleForm } from '../plugins/formToggle'
 import { plusMinus } from '../plugins/plusMinus'
 import { selector } from '../plugins/selector'
+import { timeSelector } from '../components/timeSelector'
 
 toggleForm();
 plusMinus();
 selector();
+timeSelector();
