53fa42b3184e0e3391188b98f00a54e4ab25aef7
Ben Attali <ben.attali8@gmail.com>
2019-07-09 10:48:10 +0300

    meta tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,9 @@ import { plusMinus } from '../plugins/plusMinus'
 import { toggleVideo } from '../plugins/newVideo'
 import { selector } from '../plugins/selector'
 import { timeSelector } from '../components/timeSelector'
-import { dropdown } from '../plugins/dropdown'
 
 toggleForm();
 plusMinus();
 toggleVideo();
 selector();
 timeSelector();
-dropdown();
